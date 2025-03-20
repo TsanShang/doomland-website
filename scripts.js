6908b3a119e43355b2ce7ee941572a1d1c2e7a5a
TsanShang <yt.xiaohei@gmail.com>
2025-03-20 18:10:27 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('mobile-menu');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
