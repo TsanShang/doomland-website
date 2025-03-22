4707aab2d6a77eedc0b4bd6410269033347efa43
TsanShang <yt.xiaohei@gmail.com>
2025-03-22 16:34:03 +0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const menuToggle = document.getElementById('mobile-menu');
-const nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
