1dd62c432015cbffa304c97dcc09d06929f299a7
TsanShang <yt.xiaohei@gmail.com>
2025-03-26 18:06:12 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get the hamburger menu and nav element
-const menuToggle = document.getElementById('mobile-menu');
-const nav = document.querySelector('nav');
-
-// Add event listener for the hamburger icon click event
-menuToggle.addEventListener('click', () => {
-    // Toggle the 'active' class on the nav element
-    nav.classList.toggle('active');
-});
